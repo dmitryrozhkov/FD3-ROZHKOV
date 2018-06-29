988d779c9162d638076d9c423b888e27a183bb57
Dmitry Rozhkov <dmitriy.rozhkov.by@gmail.com>
2018-06-29 20:34:59 +0300

    no changes

--- br2jsx/src/app.js
+++ br2jsx/src/app.js
@@ -25,7 +25,7 @@ class App extends React.Component {
     
       
       render () {       
-       
+      
       return (        
         <div className="text">                  
             {this.state.text}
