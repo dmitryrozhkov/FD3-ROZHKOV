8ddf798bee576bbe8336da12616cd2c4f5f4397b
dmitryrozhkov <36482305+dmitryrozhkov@users.noreply.github.com>
2018-06-30 19:00:49 +0300

    Add files via upload

---
+++ RainbowFrame/components/ColorFrame.js
@@ -0,0 +1,27 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class ColorFrame extends React.Component {
+
+  static propTypes = {
+    colors: PropTypes.array.isRequired,
+  };  
+  
+  render() {    
+    var colors = this.props.colors
+    var code = this.props.children
+  
+      
+      for (var i=0; i<colors.length; i++) {
+      
+          code = <div key = {i} style={{border:"dashed 1.5px "+colors[i],padding:"6px"}}>
+          {code}                
+          </div>         
+      }       
+      return code
+    
+  }
+
+}
+
+export default ColorFrame;
